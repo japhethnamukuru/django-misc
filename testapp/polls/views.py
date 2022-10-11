9e8f4a30f21909b811bef1ae92f1b04f9e2515f9
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-11 09:22:53 +0300

    added template files

--- testapp/polls/views.py
+++ testapp/polls/views.py
@@ -1,21 +1,24 @@
 # from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
-from .models import Question as q
+from .models import Question
 
 def index(request):
-    latest_question_list = q.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
     # output = ', '.join([question.question_text for question in latest_question_list])
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question {}".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/details.html', {'question': question})
+    # return HttpResponse("You are looking at question {}".format(question_id))
 
 def results(request, question_id):
     response = "You are looking at the results of question{}".format(question_id)
