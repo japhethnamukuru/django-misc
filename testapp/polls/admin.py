6a7d93f42c2fda0fe4995368acb8f0bad1a8b2a2
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-13 00:06:34 +0300

    updated files

--- testapp/polls/admin.py
+++ testapp/polls/admin.py
@@ -1,5 +1,26 @@
+import datetime
+from django.utils import timezone
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Data information', {'fields':['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice) 
